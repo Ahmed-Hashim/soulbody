4576f007deab916c9267415dcc08f92318683a35
Ahmed-Hashim <ah.abolaban@gmail.com>
2023-12-30 15:59:55 +0200

    domains

--- project/settings.py
+++ project/settings.py
@@ -22,10 +22,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv(
-    "DJANGO_ALLOWED_HOSTS",
-    "127.0.0.1,localhost,king-prawn-app-6aja9.ondigitalocean.app,www.soulnbody.net,soulnbody.net",
-).split(",")
+ALLOWED_HOSTS =["127.0.0.1,localhost,king-prawn-app-6aja9.ondigitalocean.app,www.soulnbody.net,soulnbody.net"]
 
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
