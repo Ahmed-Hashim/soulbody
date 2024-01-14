fabb4147436751063a3d2dac4b06dd508b3e18b6
Ahmed-Hashim <ah.abolaban@gmail.com>
2024-01-14 19:55:09 +0200

    s

--- project/wsgi.py
+++ project/wsgi.py
@@ -7,10 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 import os
+import django
 from pathlib import Path
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Call django.setup once
+django.setup(set_prefix=False)
+
 application = get_wsgi_application()
 application = WhiteNoise(application, root=os.path.join(BASE_DIR, "staticfiles"))
-
