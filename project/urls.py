3f6e4683bd9e23ba10cac5db304a8581ee32a2f1
Ahmed-Hashim <ah.abolaban@gmail.com>
2024-01-14 19:59:21 +0200

    s

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.shortcuts import redirect
 from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
