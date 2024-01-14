19ebdbd6c5d52fb82ade5467197ddeea16183ab4
Ahmed-Hashim <ah.abolaban@gmail.com>
2024-01-14 19:46:51 +0200

    s

--- project/wsgi.py
+++ project/wsgi.py
@@ -11,4 +11,4 @@ from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
 application = get_wsgi_application()
-application = WhiteNoise(application)
+#application = WhiteNoise(application)
